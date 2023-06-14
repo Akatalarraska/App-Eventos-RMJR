782a3d143dbf1c4f8d0b3f5e408781e5de17d339
Jose Real <115721093+Jrealmorillo@users.noreply.github.com>
2023-06-14 19:32:03 +0000

    añadidas tablas BBDD

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Empresa, User_Empresa, Evento, Valoracion, Factura
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,11 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Empresa, db.session))
+    admin.add_view(ModelView(User_Empresa, db.session))
+    admin.add_view(ModelView(Evento, db.session))
+    admin.add_view(ModelView(Valoracion, db.session))
+    admin.add_view(ModelView(Factura, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,9 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    dni = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +17,54 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+    
+
+class Empresa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cif = db.Column(db.String(20), unique=True, nullable=False)
+    razon_social = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    telefono = db.Column(db.Integer, unique=True, nullable=False)
+    poblacion = db.Column(db.String(40), unique=False, nullable=False)
+    direccion = db.Column(db.String(60), unique=False, nullable=False)
+    codigo_postal = db.Column(db.Integer, unique=False, nullable=False)
+    datos_bancarios = db.Column(db.String(30), unique=False, nullable=True)
+    autorizados = db.Column(db.String(60), unique=False, nullable=True)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+
+class User_Empresa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    empresa_id = db.Column(db.Integer, db.ForeignKey('empresa.id'))
+    role = db.Column(db.String(20), unique=False, nullable=False)
+  
+
+class Evento(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    empresa_id = db.Column(db.Integer, db.ForeignKey('empresa.id'))
+    nombre = db.Column(db.String(40), unique=False, nullable=False)
+    descripcion = db.Column(db.String(140), unique=False, nullable=False)
+    ubicacion = db.Column(db.String(40), unique=False, nullable=False)
+    fecha_inicio = db.Column(db.String(10), unique=False, nullable=False)
+    fecha_fin = db.Column(db.String(10), unique=False, nullable=False)
+    personas = db.Column(db.Integer, unique=False, nullable=False)
+    free = db.Column(db.Boolean())
+    importe = db.Column(db.Integer, unique=False, nullable=False)
+
+
+class Valoracion(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    evento_id = db.Column(db.Integer, db.ForeignKey('evento.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    estrellas = db.Column(db.Integer, unique=False, nullable=False)
+    comentario = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Factura(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    evento_id = db.Column(db.Integer, db.ForeignKey('evento.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    fecha = db.Column(db.String(10), unique=False, nullable=False)
+    pasarela_id = db.Column(db.String(120), unique=True, nullable=False)
