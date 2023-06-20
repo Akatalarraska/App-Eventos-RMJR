a981ba55b841b6b55ab0197936b7dd835dedecb0
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-06-20 20:58:29 +0000

    navbar modificado, pndt terminar estilo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,41 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const toggleMenu = () => {
+  document.body.classList.toggle("open");
+};
+
 export const Navbar = () => (
-	<nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-		<a className="navbar-brand" href="/">
-			<img
-			alt="Navbar logo"
-			style={{ height: '80px', width: '150px' }}
-			src="https://trello.com/1/cards/6487673b8a84db1e093e494c/attachments/648d75a034e7c5e96de2b244/previews/648d75a034e7c5e96de2b295/download/image.png"
-		/>
-		</a>
-		<button
-			className="navbar-toggler"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarNav"
-			aria-controls="navbarNav"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<span className="navbar-toggler-icon"></span>
-		</button>
-		<div className="collapse navbar-collapse" id="navbarNav">
-			<ul className="navbar-nav ms-auto">
-				<li className="nav-item mx-2">
-					<a href="/destinations" className="btn btn-outline-primary">Home</a>
-				</li>
-				<li className="nav-item mx-2">
-					<a href="/day-tours" className="btn btn-outline-primary">Contact</a>
-				</li>
-				<li className="nav-item mx-2">
-					<a href="/about" className="btn btn-outline-primary">Private Area</a>
-				</li>
-				<li className="nav-item mx-2">
-					<button className="btn btn-outline-primary">Sign Up / Log In</button>
-				</li>
-			</ul>
-		</div>
-  </nav>
+  <div className="container">
+    <nav>
+      <img src="ruta_de_la_imagen.jpg" alt="Logo" />
+    </nav>
+    <div className="overlay"></div>
+    <button className="burger" onClick={toggleMenu}>
+      <i className="fa-solid fa-bars"></i>
+      <i className="fa-solid fa-close"></i>
+    </button>
+    <aside>
+      <a>Home</a>
+      <a>Products</a>
+      <a>Contact</a>
+      <h3>Test</h3>
+      <a>Private Area</a>
+      <button>Sign Up // Log in</button>
+    </aside>
+  </div>
 );
