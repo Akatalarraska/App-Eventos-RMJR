ae1f632cc4649e9d5c29c3ed6c19e4c623a2eb85
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-06-30 17:17:55 +0000

    flipcards creadas con estilos, pndt meter en carrusel

--- src/front/js/component/expandingCards.js
+++ src/front/js/component/expandingCards.js
@@ -1,62 +1,56 @@
 import React, { useEffect, useState } from "react";
-import "../../styles/home.css";
 import "../../styles/expandingCards.css";
 
-
-
 export const ExpandingCards = () => {
+  const [eventos, setEventos] = useState([]);
 
-	const [eventos, setEventos] = useState([]);
-    
-    useEffect(() => {
-		fetch("https://akatalarraska-laughing-spoon-wrv5vv69wx93v57q-3001.preview.app.github.dev//api/eventos")
-		  .then(response => {
-			if (response.ok) {
-			  return response.json();
-			}
-			throw new Error("Error de respuesta: " + response.status);
-		  })
-		  .then(data => {
-			setEventos(data);
-		  })
-		  .catch(error => {
-			console.error(error);
-		  });
-	  }, []);
-    
-    
-    return(
-        <>
-            <div className="eventcard">
-					<div className="container marketing">
-						<div className="row">
-						{eventos.map(evento => (
-							<div className="col-lg-3 col-sm-6" key={evento.id}>
-							<img
-								className="bd-placeholder-img rounded"
-								width="240"
-								height="200"
-								src={evento.imagen}
-								role="img"
-							/>
-							<h2 className="fw-normal">{evento.nombre}</h2>
-							<p>{evento.descripcion}</p>
-							</div>
-						))}
-						</div>
-					</div>
-				</div>
+  useEffect(() => {
+    fetch("https://akatalarraska-laughing-spoon-wrv5vv69wx93v57q-3001.preview.app.github.dev//api/eventos")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Error de respuesta: " + response.status);
+      })
+      .then(data => {
+        setEventos(data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-				<div className="icard" style={{clr: "#009688"}}>
-					<div className="img-box">
-						<img src=""/>
-					</div>
-					<div className="content">
-						<h2>Nombre</h2>
-						<p>descripcion</p>
-					</div>
-				</div>	
-
-        </>
-    )
-}
+  return (
+    <div className="eventcard">
+      <div className="container marketing">
+        <div className="row">
+          {eventos.map(evento => (
+            <div className="col-lg-3 col-sm-6" key={evento.id}>
+              <div className="flip-card">
+                <div className="flip-card-inner">
+                  <div className="flip-card-front">
+                    <img
+                      className="bd-placeholder-img rounded"
+                      width="240"
+                      height="200"
+                      src={evento.imagen}
+                      alt={evento.nombre}
+                    />
+                  </div>
+                  <div className="flip-card-back">
+                    <h6 className="fw-normal pt-2">{evento.descripcion}</h6>
+					<p>{evento.ubicacion}</p>
+					<p>{evento.fecha_inicio + "~" + evento.fecha_fin}</p>
+					<p>{evento.personas} personas</p>
+                  </div>
+                </div>
+              </div>
+              <h2 className="fw-normal">{evento.nombre}</h2>
+              
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
