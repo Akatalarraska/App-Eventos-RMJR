16e09c3f439a4194df873d665f715a02583e0ab6
Akatalarraska <125604224+Akatalarraska@users.noreply.github.com>
2023-07-07 18:38:30 +0000

    valoracion evento buttons creados, pndt sincronizar con la api BD

--- src/front/js/pages/event.js
+++ src/front/js/pages/event.js
@@ -55,9 +55,6 @@ export const Event = () => {
                             <p>{evento.free} It's free?</p>
                             <p>{evento.importe} €</p>
                             <div className="botones">
-                              <button className="btn btn-primary px-5 mb-5" type="button">
-                              Registration
-                              </button>
                               <button className="buttoncom" onClick={handleCommentClick}>
                                 <svg className="svg-icon" fill="none" height="22" viewBox="0 0 20 20" width="22" xmlns="http://www.w3.org/2000/svg"><g stroke="#fff" strokeLinecap="round" strokeWidth="1.5"><path d="m6.66669 6.66667h6.66671"></path><path clipRule="evenodd" d="m3.33331 5.00001c0-.92047.74619-1.66667 1.66667-1.66667h10.00002c.9205 0 1.6666.7462 1.6666 1.66667v6.66669c0 .9205-.7461 1.6666-1.6666 1.6666h-4.8274c-.1105 0-.21654.044-.29462.122l-2.50004 2.5c-.26249.2625-.71129.0766-.71129-.2945v-1.9108c0-.2301-.18655-.4167-.41667-.4167h-1.25c-.92048 0-1.66667-.7461-1.66667-1.6666z" fillRule="evenodd"></path><path d="m6.66669 10h2.5"></path></g></svg>
                                 <span className="lable">Comment</span>
@@ -147,8 +144,13 @@ export const Event = () => {
                                     value={comment}
                                     onChange={(e) => setComment(e.target.value)}
                                   ></textarea>
-                                  <button onClick={handleCommentSubmit}>Submit</button>
-                                  <button onClick={() => setShowCommentForm(false)}>Cancel</button>
+                                  <div className="botones">
+                                    <button className="buton" onClick={handleCommentSubmit}> Submit
+                                      <span></span>
+                                    </button>
+                                    
+                                    <button className="buton" id="cancel" onClick={() => setShowCommentForm(false)}>Cancel<span></span></button>
+                                  </div>
                                 </div>
                               </div>
                             )}
