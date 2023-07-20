9faf220b3a9ea074378f6a134cb8ce43b72e20c4
Jose Real <115721093+Jrealmorillo@users.noreply.github.com>
2023-07-20 17:54:19 +0000

    add datepicker calendar

--- src/front/js/pages/createevent.js
+++ src/front/js/pages/createevent.js
@@ -5,8 +5,8 @@ import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 import "../../styles/createevent.css";
 import Swal from "sweetalert2";
-
-
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 
 export const CreateEvent = () => {
@@ -24,6 +24,17 @@ export const CreateEvent = () => {
     const { store, actions } = useContext(Context);
 
     const navigate = useNavigate();
+
+
+
+    const handleFechaInicioChange = (date) => {
+        setFechaInicio(date);
+    };
+
+
+    const handleFechaFinChange = (date) => {
+        setFechaFin(date);
+    };
 
 
     const handleCreateEventSubmit = async (e) => {
@@ -44,14 +55,14 @@ export const CreateEvent = () => {
 
             if (success) {
                 await Swal.fire({
-                    title: 'Éxito',
+                    title: 'Enhorabuena',
                     text: 'Evento creado correctamente',
                     icon: 'success',
                     confirmButtonText: 'Aceptar'
                 });
                 navigate('/private');
             } else {
-                throw new Error("Error al crear el evento");
+                throw new Error("No se pudo crear el evento");
             }
         } catch (error) {
             await Swal.fire({
@@ -89,7 +100,7 @@ export const CreateEvent = () => {
                             Crea tu evento
                         </h3>
                         <div className="row m-3 d-flex justify-content-center " >
-                            <div className="col-8 col-xl-10 col-lg-10">
+                            <div className="col-8 col-xl-10 col-lg-10 form-group">
                                 <label htmlFor="nombre" className='ms-1'>Nombre del evento</label>
                                 <input
                                     type="text"
@@ -105,7 +116,7 @@ export const CreateEvent = () => {
                             </div>
                         </div>
                         <div className="row m-3 d-flex justify-content-center ">
-                            <div className="col-8 col-xl-10 col-lg-10">
+                            <div className="col-8 col-xl-10 col-lg-10 form-group">
                                 <label htmlFor="descripcion" className='ms-1'>Describe tu evento</label>
                                 <textarea
                                     id="descripcion"
@@ -120,7 +131,7 @@ export const CreateEvent = () => {
                             </div>
                         </div>
                         <div className="row m-3 d-flex justify-content-center " >
-                            <div className="col-8 col-xl-10 col-lg-10">
+                            <div className="col-8 col-xl-10 col-lg-10 form-group">
                                 <input
                                     id="formFile"
                                     name="imagen"
@@ -134,7 +145,7 @@ export const CreateEvent = () => {
                             </div>
                         </div>
                         <div className="row m-3 d-flex justify-content-center " >
-                            <div className="dropdown col-8 col-xl-10 col-lg-10">
+                            <div className="dropdown col-8 col-xl-10 col-lg-10 form-group">
                                 <label htmlFor="ubicacion">Ubicación</label>
                                 <select
                                     id="ubicacion"
@@ -152,45 +163,41 @@ export const CreateEvent = () => {
                                 </select>
                             </div>
                         </div>
-                        <div className="row m-3 d-flex justify-content-center align-items-end" >
-                            <div className="col-4 col-xl-5 col-lg-5">
-                                <label htmlFor="Fecha de inicio" className='ms-1'>Fecha de inicio</label>
-                                <input
-                                    type="date"
-                                    id="Fecha de inicio"
-                                    name="Fecha de inicio"
+                        <div className="row m-3 d-flex justify-content-center" >
+                            <div className="col-4 col-xl-5 col-lg-5 form-group">
+                                <label htmlFor="Fecha de inicio" className='m-1'>Fecha de inicio</label>
+                                <DatePicker
+                                    showIcon
+                                    selected={fechaInicio}
+                                    dateFormat={"dd/MM/yyyy"}
+                                    placeholderText='dd/mm/aaaa'
+                                    onChange={handleFechaInicioChange}
+                                    minDate={new Date()}
                                     className="form-control form-control-event-create form-control-lg"
-                                    placeholder="Fecha de inicio"
-                                    required
-                                    value={fechaInicio}
-                                    onChange={(event) => setFechaInicio(event.target.value)
-                                    }
                                 />
+
                             </div>
-                            <div className="col-4 col-xl-5 col-lg-5">
-                                <label htmlFor="Fecha de fin" className='ms-1'>Fecha de fin</label>
-                                <input
-                                    type="date"
-                                    id="Fecha de fin"
-                                    name="Fecha de fin"
+                            <div className="col-4 col-xl-5 col-lg-5 form-group">
+                                <label htmlFor="Fecha de fin" className='m-1'>Fecha de fin</label>
+                                <DatePicker
+                                    showIcon
+                                    selected={fechaFin}
+                                    dateFormat={"dd/MM/yyyy"}
+                                    placeholderText='dd/mm/aaaa'
+                                    onChange={handleFechaFinChange}
+                                    minDate={fechaInicio} 
                                     className="form-control form-control-event-create form-control-lg"
-                                    placeholder="Fecha de fin"
-                                    required
-                                    value={fechaFin}
-                                    onChange={(event) => setFechaFin(event.target.value)
-                                    }
                                 />
                             </div>
                         </div>
                         <div className="row m-3 d-flex justify-content-center align-items-end" >
-                            <div className="col-4 col-xl-5 col-lg-5">
+                            <div className="col-4 col-xl-5 col-lg-5 form-group">
                                 <label htmlFor="personas" className='ms-1'>Número de asistentes</label>
                                 <input
                                     type="number"
                                     id="personas"
                                     name="personas"
                                     className="form-control form-control-event-create form-control-lg"
-                                    placeholder="Número de personas"
                                     required
                                     min="1"
                                     value={personas}
@@ -198,7 +205,7 @@ export const CreateEvent = () => {
                                     }
                                 />
                             </div>
-                            <div className="col-4 col-xl-5 col-lg-5">
+                            <div className="col-4 col-xl-5 col-lg-5 form-group">
                                 <label htmlFor="Importe" className='ms-1'>Importe por persona</label>
                                 <input
                                     type="number"
@@ -215,7 +222,7 @@ export const CreateEvent = () => {
                             </div>
                         </div>
                         <div className="row m-3 align-items-end" >
-                            <div className="col-8 col-xl-10 col-lg-10">
+                            <div className="col-8 col-xl-10 col-lg-10 form-group">
                                 <div className="form-check d-flex align-items-center">
                                     <input
                                         className="form-check-input"
@@ -244,165 +251,3 @@ export const CreateEvent = () => {
         </div>
     )
 }
-
-
-
-// const handleFileChange = async (event) => {
-//     event.preventDefault();
-//     const file = event.target.files[0];
-//     setImagen(file);
-//     console.log("These are the files", file);
-//     let body = new FormData();
-//     body.append("imagen", file);
-//     const options = {
-//       method: "POST",
-//       body
-//     };
-
-//     try {
-//       const response = await fetch(process.env.BACKEND_URL + "/api/crearevento", options);
-//       if (response.ok) {
-//         const data = await response.json();
-//         console.log("Success!!!!", data);
-//       } else {
-//         throw new Error("Error uploading image");
-//       }
-//     } catch (error) {
-//       console.error("ERRORRRRRR!!!", error);
-//     }
-//   };
-
-
-
-// const handleFileChange = async (event) => {
-//     event.preventDefault();
-//     const file = event.target.files[0];
-//     setImagen(file);
-//     console.log("These are the files", file);
-
-//     const formData = new FormData();
-//     formData.append("imagen", file);
-//     formData.append("nombre", nombre);
-//     formData.append("descripcion", descripcion);
-//     formData.append("ubicacion", ubicacion);
-//     formData.append("fechaInicio", fechaInicio);
-//     formData.append("fechaFin", fechaFin);
-//     formData.append("personas", personas);
-//     formData.append("free", free);
-//     formData.append("importe", importe);
-
-//     const options = {
-//         method: "POST",
-//         body: formData
-//     };
-
-//     try {
-//         const response = await fetch(process.env.BACKEND_URL + "/api/crearevento", options);
-//         if (response.ok) {
-//             const data = await response.json();
-//             console.log("Success!!!!", data);
-//         } else {
-//             throw new Error("Error uploading image");
-//         }
-//     } catch (error) {
-//         console.error("ERRORRRRRR!!!", error);
-//     }
-// };
-
-
-
-// const handleCreateEventSubmit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-//         const success = await actions.createEvent(
-//             nombre,
-//             descripcion,
-//             imagen,
-//             ubicacion,
-//             fechaInicio,
-//             fechaFin,
-//             personas,
-//             free,
-//             importe
-//         );
-
-//         if (success) {
-//             await Swal.fire({
-//                 title: 'Éxito',
-//                 text: 'Evento creado correctamente',
-//                 icon: 'success',
-//                 confirmButtonText: 'Aceptar'
-//             });
-//             navigate('/private');
-//         } else {
-//             throw new Error("Error al crear el evento");
-//         }
-//     } catch (error) {
-//         await Swal.fire({
-//             title: 'Error',
-//             text: error.message,
-//             icon: 'error',
-//             confirmButtonText: 'Aceptar'
-//         });
-//     }
-// };
-
-
-
-
-// const handleFileChange = (event) => {
-//     event.preventDefault()
-//     const file = event.target.files[0];
-//     setImagen(file);
-// };
-
-/////////////////// IMAGEN DEL FORMULARIO CREAR EVENTO //////////////////////
-
-{/* <div className="col-xl-8 col-lg-8 text-align-center">
-                    <img src={eventImage} alt="Imagen de registro de usuario" className="img-create-event" />
-                </div> */}
-
-
-/////////////////// INPUT PARA SUBIR ARCHIVO (REVISAR, NO FUNCIONA) //////////////////////
-
-//     <div className="row m-3 d-flex justify-content-center " >
-//     <div className="col-xl-8 col-lg-8">
-//         <input
-//             id="formFile"
-//             name="imagen"
-//             className="form-control form-control-event-create form-control-lg"
-//             placeholder="Imagen del evento"
-//             type="file"
-//             accept='image/*'
-//             defaultValue={imagen ? imagen.name : ""}
-//             onChange={handleFileChange}
-//         />
-//     </div>
-// </div>
-
-
-////////////////  FUNCION PARA GESTIONAR EL ARCHIVO IMAGEN /////////////////
-
-// const handleFileChange = (event) => {
-//     const file = event.target.files[0];
-//     setImagen(file);
-// };
-
-
-/////////////////// INPUT PARA SUBIR IMAGEN EN URL (OK FUNCIONANDO) //////////////////////
-
-{/* <div className="row m-3 d-flex justify-content-center " >
-                            <div className="col-xl-10 col-lg-10">
-                                <label htmlFor="imagen" className='ms-1'>Imagen del evento</label>
-                                <input
-                                    id="imagen"
-                                    name="imagen"
-                                    className="form-control form-control-event-create form-control-lg"
-                                    placeholder="(URL)"
-                                    type="text"
-                                    value={imagen}
-                                    onChange={(event) => setImagen(event.target.value)}
-                                />
-                            </div>
-                        </div> */}
